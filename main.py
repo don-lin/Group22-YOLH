da807b9eb1a897965e8e2352dbafa3fdc9057153
don-lin <chendonglin@emails.bjut.edu.cn>
2021-03-01 23:53:48 +0800

    新增：chart.js绘制图表

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask,send_from_directory
 
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder='static')
 
 @app.route('/')
 def hello_world():
@@ -10,6 +10,11 @@ def hello_world():
 @app.route('/map')
 def send_map():
     return send_from_directory('.','地址解析.html')
+
+@app.route('/page/<path:temp>')
+def send_static_file(temp):
+    print('hello!')
+    return send_from_directory("web",temp)
 
 if __name__ == '__main__':
     app.run(debug=True)
